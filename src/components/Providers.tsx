322f94fe8391bca3bd78109439189223f7ce681f
Maximo <pachecomaximo11@gmail.com>
2023-10-31 10:53:06 -0300

    navbar - trpc setup - auth

---
+++ src/components/Providers.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { useState } from "react"
+import { QueryClient } from "@tanstack/react-query"
+
+const Providers = () => {
+
+	const [queryClient] = useState(() => new QueryClient())
+	const [trpcClient] = useState(() =>)
+
+	return (
+		<div>Providers</div>
+	)
+}
+
+export default Providers
