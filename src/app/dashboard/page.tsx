ea69f71eb64af850e0a74e3aaa13fa69a363bfc5
Maximo <pachecomaximo11@gmail.com>
2023-11-02 10:58:00 -0300

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
-import { db } from "@/db";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { redirect } from "next/navigation";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+
+import { db } from "@/db";
+import Dashboard from "@/components/Dashboard";
 
 const Page = async() => {
 	
@@ -17,9 +19,7 @@ const Page = async() => {
 
 	if(!dbUser) redirect("/auth-callback?origin=dashboard")
 
-	return (
-		<div>{user.email}</div>
-	)
+	return <Dashboard /> 
 }
 
 export default Page
